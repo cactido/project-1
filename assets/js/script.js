420879ce81540ad23c2319c044b9d48a2a72cf46
Ryely <ryely.moore@outlook.com>
2021-06-04 00:06:01 -0600

    tested and added some more apis that could be fun or useful to the project

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+// Fetches a random joke could expand to give the user the ability to filter by programming or general?
+// Documentation here: https://github.com/15Dkatz/official_joke_api
 var jokeRandom = function() {
     fetch("https://official-joke-api.appspot.com/random_joke")
     .then(function(res) {
@@ -6,7 +8,8 @@ var jokeRandom = function() {
         });
     });
 }
-
+// Gets random trivia question could give user option to filter by category, true false, etc.
+// Documentation here: https://opentdb.com/api_config.php
 var triviaRandom = function() {
     fetch("https://opentdb.com/api.php?amount=1").then(function(res){
         res.json().then(function(data){
@@ -15,6 +18,8 @@ var triviaRandom = function() {
     })
 }
 
+// Grabs a random XKCD comic. Currently having and issue with CORS so currently implementing this is on the backburner
+// Documentation here:
 var randomXKCD = function() {
     // creates a random number between 0-614
     var xRand = Math.floor(Math.random()*615);
@@ -35,5 +40,42 @@ var randomXKCD = function() {
     });
 }
 
-var x = [jokeRandom, triviaRandom, randomXKCD];
+// fetches a random activity, can display this as "Random Activity for Later" and maybe put in options to filter by type
+// documentation here: https://www.boredapi.com/documentation
+var randomActivity = function() {
+    fetch("http://www.boredapi.com/api/activity/").then(function(res){
+        res.json().then(function(data){
+            console.log(data);
+        })
+    })
+}
+// Gets a Random picture of food
+// Documentation Here: https://github.com/surhud004/Foodish#readme
+var randomFoodPic = function() {
+    fetch("https://foodish-api.herokuapp.com/api/").then(function(res){
+        res.json().then(function(data){
+            console.log(data);
+        })
+    })
+}
+// Get some random advice
+// Documentation Here: https://api.adviceslip.com/#top
+var randomAdvice = function() {
+    fetch("https://api.adviceslip.com/advice").then(function(res){
+        res.json().then(function(data){
+            console.log(data);
+        })
+    })
+}
+// Gets a random Quote
+// Documentation Here: https://github.com/lukePeavey/quotable
+var randomQuote = function() {
+    fetch("https://api.quotable.io/random").then(function(res){
+        res.json().then(function(data){
+            console.log(data);
+        })
+    })
+}
+
+var x = [jokeRandom, triviaRandom, randomActivity, randomFoodPic, randomAdvice, randomQuote];
 
